138d29c9991ea884eb90b2d3789c214b0b257791
clintev1 <75999225+clintev1@users.noreply.github.com>
2021-03-08 17:42:27 +0530

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,6 +1,4 @@
-function hiddencookie() {
-
-    function setCookie(cname, cvalue, exdays) {
+function setCookie(cname, cvalue, exdays) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         var expires = "expires="+d.toUTCString();
@@ -34,4 +32,3 @@ function hiddencookie() {
                  }
                 }
                }
-              }
