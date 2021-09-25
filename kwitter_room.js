ee72899b662a3d8e6ac43544764833ec2799a33e
clintev1 <75999225+clintev1@users.noreply.github.com>
2021-09-25 09:22:57 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -10,6 +10,18 @@
   };
   firebase.initializeApp(firebaseConfig);
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC9BQYHRLhdkbnUG3XXTM5nbhJlTP2rprc",
+  authDomain: "kwitter-original.firebaseapp.com",
+  projectId: "kwitter-original",
+  storageBucket: "kwitter-original.appspot.com",
+  messagingSenderId: "179727529964",
+  appId: "1:179727529964:web:6c5b743ab7f1b32642f2c9",
+  measurementId: "G-FZ1XH00YNN"
+};
+
+const app = initializeApp(firebaseConfig);
+
   user_name = localStorage.getItem("user_name");
 
 document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
